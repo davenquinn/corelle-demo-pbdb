785f06427a943e769d63510ae093087f8a75a69a
Daven Quinn <dev@davenquinn.com>
2020-08-13 01:14:33 -0500

    Hello world

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+// https://paleobiodb.org/data1.2/colls/summary.json?show=time&min_ma=10&max_ma=12&level=3
+import React from "react";
+import { render } from "react-dom";
+
+render(<h1>Hello World</h1>, document.getElementById("root"));
