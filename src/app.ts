24dbf608bb890cad4f6c9fb3c4255887b115eaff
Daven Quinn <dev@davenquinn.com>
2020-08-26 21:38:52 -0500

    Nice looking and configurable

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,51 @@ import h from "@macrostrat/hyper";
 
 import { useState, useEffect } from "react";
 import { Map } from "./map";
+import { ResizeSensor } from "@blueprintjs/core";
 import { RotationsProvider } from "@macrostrat/corelle";
+import { Timescale } from "@macrostrat/timescale";
+import "@macrostrat/timescale/dist/timescale.css";
 
 function App(props) {
   const model = "Wright2013";
-  const time = 300;
+  const [time, setTime] = useState(300);
 
-  return h("div", [
-    h(RotationsProvider, { model, time, debounce: 1000 }, h(Map)),
+  const [size, setSize] = useState({
+    width: 1100,
+    height: 800,
+  });
+
+  function onResize(entries) {
+    const { width, height } = entries[0].contentRect;
+    return setSize({ width, height });
+  }
+
+  const { width, height } = size;
+
+  return h(ResizeSensor, { onResize }, [
+    h("div.app", [
+      h(RotationsProvider, { model, time, debounce: 1000 }, [
+        h(Map, { width, height: height - 100 }),
+      ]),
+      h(Timescale, {
+        ageRange: [542, 0],
+        orientation: "horizontal",
+        length: size.width - 20,
+        absoluteAgeScale: true,
+        rootInterval: 751,
+        levels: [2, 3],
+        cursorPosition: time,
+        axisProps: {
+          orientation: "top",
+          tickLength: 4,
+          hideAxisLine: true,
+          labelOffset: 10,
+        },
+        onClick(event, age) {
+          setTime(age);
+        },
+      }),
+    ]),
   ]);
 }
 
--- src/map.ts
+++ src/map.ts
@@ -1,5 +1,3 @@
-import { ResizeSensor } from "@blueprintjs/core";
-import { useState } from "react";
 import { geoNaturalEarth1 } from "d3-geo";
 import { PlateFeatureLayer } from "@macrostrat/corelle";
 import { hyperStyled } from "@macrostrat/hyper";
@@ -11,46 +9,33 @@ import styles from "./main.styl";
 const h = hyperStyled(styles);
 
 function Map(props) {
-  const [size, setSize] = useState({
-    width: 1100,
-    height: 800,
-  });
-
-  function onResize(entries) {
-    const { width, height } = entries[0].contentRect;
-    return setSize({ width, height });
-  }
-
-  const { width, height } = size;
+  const { width, height } = props;
   const projection = geoNaturalEarth1().precision(0.5);
 
   return h(
-    ResizeSensor,
-    { onResize },
-
+    "div.world-map",
+    null,
     h(
-      "div.world-map",
-      null,
-      h(
-        Globe,
-        {
-          keepNorthUp: true,
-          projection,
-          width,
-          height,
-          scale: Math.min(width / 5.5, height / 3) - 10,
-        },
-        [
-          //h(PlatePolygons),
-          h(PlateFeatureLayer, {
-            name: "ne_110m_land",
-            style: {
-              fill: "#E9FCEA",
-              stroke: "#9dc99f",
-            },
-          }),
-        ]
-      )
+      Globe,
+      {
+        keepNorthUp: true,
+        projection,
+        width,
+        height,
+        allowZoom: false,
+        //allowDrag: false,
+        scale: Math.min(width / 5.5, height / 3) - 10,
+      },
+      [
+        //h(PlatePolygons),
+        h(PlateFeatureLayer, {
+          name: "ne_110m_land",
+          style: {
+            fill: "#E9FCEA",
+            stroke: "#9dc99f",
+          },
+        }),
+      ]
     )
   );
 }
