7ae044c6351ca91548a9f8438429e6c633adda73
Daven Quinn <dev@davenquinn.com>
2021-08-09 16:21:13 -0400

    Updated demo for new corelle endpoint:

--- src/credits.ts
+++ src/credits.ts
@@ -1,11 +1,12 @@
 import h from "@macrostrat/hyper";
+import { version } from "../package.json"
 
 function Credits() {
   return h("div.credits", [
     h("h1", "Corelle"),
     h("p", "Client-side rotation of PBDB collections."),
     h("p", [h("a.author", { href: "https://davenquinn.com" }, "Daven Quinn")]),
-    h("p", [h("span.version", "v1.0.0"), ", ", h("span.date", "Aug. 2020")]),
+    h("p", [h("span.version", `v${version}`), ", ", h("span.date", "August 2021")]),
     h("p", [
       h(
         "a",
